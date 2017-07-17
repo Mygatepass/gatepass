0caddaa82f91044040a4a8ec6e51d30c558b5e02
Anuj-786 <anuj.k@gai.co.in>
2017-07-17 16:53:33 +0530

    Working Version: Adding the responsive design

--- materialjquery.js
+++ materialjquery.js
@@ -4,4 +4,21 @@
  		menuWidth: 550,
  	});
  	$('.modal').modal();
+ 	$("select[required]").css({
+      display: "inline",
+      height: 0,
+      padding: 0,
+      width: 0
+    });
+ 	$('#myButton').click(function(){
+    if( $('#name').val() == '' || $('#contact').val() =='' || $('#email').val() ==''){
+      alert('Please Enter all the fields');
+        return false;
+    }
+    else {
+    	$('#modal1').modal('open');
+    }
+
+});
+	
  });
