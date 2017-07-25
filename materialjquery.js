3f6b4defe21079329f96f3e63f0472658ab74db9
Anuj-786 <anuj.k@gai.co.in>
2017-07-25 18:33:51 +0530

    Working Version: Adding Scroll for navigation menues

--- materialjquery.js
+++ materialjquery.js
@@ -1,15 +1,15 @@
  $(document).ready(function() {
- 
+
  	$(".button-collapse").sideNav({
  		menuWidth: 550,
  	});
  	$('.modal').modal();
  	$("select[required]").css({
-      display: "inline",
-      height: 0,
-      padding: 0,
-      width: 0
-    });
+    display: "inline",
+    height: 0,
+    padding: 0,
+    width: 0
+  });
  	$('#myButton').click(function(){
     if( $('#name').val() == '' || $('#contact').val() =='' || $('#email').val() ==''){
       alert('Please Enter all the fields');
@@ -19,6 +19,42 @@
     	return false;
     }
 
-});
-	
+  });
+
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
  });
